407c90c5aa4daa1c594726316a444e1fe6cc7512
Andrew Bieber <andrewbieber.work@gmail.com>
2026-09-11 20:51:54 -0500

    All 4 chunk sizes all embedded!

--- 001_rag/src/embedder.py
+++ 001_rag/src/embedder.py
@@ -2,13 +2,13 @@
 # RAG Reproduction — Embeddings assigned
 #
 # Author: Andrew Bieber <andrewbieber.work@gmail.com>
-# Last Updated: September 8, 2026
+# Last Updated: September 11, 2026
 #
 # File: embedder.py
 #
 # Description:
 # Reads all chunks from each set of different chunk sizes
-# and dumps the binary vectors into data/embeddings/*pt.
+# and dumps the bdense floating-point vectors into data/embeddings/*pt.
 # We use the pretrained weights and BPE encoder of DPR given to
 # us by the wonderful people who wrote this paper lol.
 # YAY OPEN_SOURCE <3
@@ -21,6 +21,8 @@
 
 import torch
 from transformers import DPRContextEncoder, DPRContextEncoderTokenizer
+import json
+
 
 tokenizer = DPRContextEncoderTokenizer.from_pretrained(
     "facebook/dpr-ctx_encoder-single-nq-base"
@@ -30,15 +32,49 @@ model = DPRContextEncoder.from_pretrained(
     "facebook/dpr-ctx_encoder-single-nq-base"
 )
 
-"""
-This is a test to see we can represent one chunk with embeddings using the
-actual open source resources.
-"""
-# text = "A balk is an illegal move in baseball"
+# Switch model from training mode -> inference mode
+model.eval()
 
-# tokens = tokenizer(text, return_tensors="pt")
 
-# embeddings = model(**tokens).pooler_output
+# First we have to actually read the jsonl file. Its read as a list of strings
+def load_lines(file_path: str) -> list[str]:
+    with open(file_path, "r", encoding="utf8") as f:
+        lines = f.readlines()
 
-# print(embeddings.shape)
-# print(embeddings)
+    return lines
+    # embeddings = torch.tensor(len(lines), 768)
+    # for line in lines
+
+# Helper function that turns a string -> dict from our loaded jsonl lines
+def parse_line(line: str) -> dict:
+    hashmap = json.loads(line)
+    return hashmap
+
+# Uses parse_line to loop through all lines and convert strings -> dicts
+# Stored as a list of dicts
+def process_all_lines(lines: list[str]) -> list[dict]:
+    processed = []
+    for line in lines:
+        processed.append(parse_line(line))
+
+    return processed
+
+def embed_chunk(file_path: str, save_path: str) -> None:
+    lines = load_lines(file_path)
+    processed = process_all_lines(lines)
+
+    # Can just declare our embedding shapes here
+    embeddings = torch.empty(len(lines), 768)
+
+    for i, line in enumerate(processed):
+        tokens = tokenizer(line["text"], return_tensors="pt")
+        embed = model(**tokens).pooler_output
+        embeddings[i] = embed.squeeze(0)
+
+    torch.save(embeddings, save_path)
+
+if __name__ == "__main__":
+
+    # All chunks are located here
+    save_name = "data/embeddings/embeddings_size100.pt"
+    embed_chunk("data/chunks/chunks_size100.jsonl", save_name)
