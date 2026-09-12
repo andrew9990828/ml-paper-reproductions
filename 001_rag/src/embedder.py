1111aaf12de872c8b131b2722dfa4b7a2eaeaf29
Andrew Bieber <andrewbieber.work@gmail.com>
2026-09-11 19:16:38 -0500

    Test of loading open source tokenizers and embedding tools

--- 001_rag/src/embedder.py
+++ 001_rag/src/embedder.py
@@ -19,3 +19,26 @@
 # https://arxiv.org/abs/2005.11401
 # ============================================================
 
+import torch
+from transformers import DPRContextEncoder, DPRContextEncoderTokenizer
+
+tokenizer = DPRContextEncoderTokenizer.from_pretrained(
+    "facebook/dpr-ctx_encoder-single-nq-base"
+)
+
+model = DPRContextEncoder.from_pretrained(
+    "facebook/dpr-ctx_encoder-single-nq-base"
+)
+
+"""
+This is a test to see we can represent one chunk with embeddings using the
+actual open source resources.
+"""
+# text = "A balk is an illegal move in baseball"
+
+# tokens = tokenizer(text, return_tensors="pt")
+
+# embeddings = model(**tokens).pooler_output
+
+# print(embeddings.shape)
+# print(embeddings)
