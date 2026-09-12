e65ac1006877856db901cba832994c04806464e8
Andrew Bieber <andrewbieber.work@gmail.com>
2026-09-12 15:54:39 -0500

    Fixed tiny bug in embedder.py

--- 001_rag/src/embedder.py
+++ 001_rag/src/embedder.py
@@ -77,6 +77,7 @@ if __name__ == "__main__":
 
     # All chunks are located here
     save_name = "data/embeddings/embeddings_size100.pt"
-    with torch.inference_mode(
+    with torch.inference_mode():
+        (
         embed_chunk("data/chunks/chunks_size100.jsonl", save_name)
     )
